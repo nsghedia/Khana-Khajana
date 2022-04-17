c627cc195f178a4352ad5d272e6ada9a74d22a13
Neha Ghedia <nsghedia@gmail.com>
2022-04-17 17:14:11 +0530

    Passing Data with Event and Property Binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,24 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-
-  recipes:Recipe[] = [
-    new Recipe('Uttapama','this is not a uttapama','https://picsum.photos/id/237/200/300'),
-    new Recipe('Dhokla','this is not a dhokla','https://picsum.photos/id/237/200/300'),
-    new Recipe('Handva','this is not a handva','https://picsum.photos/id/237/200/300'),
-    new Recipe('Uttapama','this is not a uttapama','https://picsum.photos/id/237/200/300'),
-    new Recipe('Pani-puri','this is not a pani-puri','https://picsum.photos/id/237/200/300')
+  recipes: Recipe[] = [
+    new Recipe(
+      'Uttapama',
+      'this is not a uttapama',
+      'https://picsum.photos/id/237/200/300'
+    ),
+    new Recipe(
+      'Dhokla',
+      'this is not a dhokla',
+      'https://picsum.photos/id/237/200/300'
+    ),
+    new Recipe(
+      'Handva',
+      'this is not a handva',
+      'https://picsum.photos/id/237/200/300'
+    ),
+    new Recipe(
+      'Uttapama',
+      'this is not a uttapama',
+      'https://picsum.photos/id/237/200/300'
+    ),
+    new Recipe(
+      'Pani-puri',
+      'this is not a pani-puri',
+      'https://picsum.photos/id/237/200/300'
+    ),
   ];
+  @Output() recipeWasSelected = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
-
 }
