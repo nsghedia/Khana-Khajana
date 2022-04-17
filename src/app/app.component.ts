aba6258fe357488baa5e361e45d453d1d5c014fa
Neha Ghedia <nsghedia@gmail.com>
2022-04-17 22:28:22 +0530

    What Happens behind the Scenes on Structural Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Khana Khajana';
   loadedFeature: string = 'recipe';
+
+  id: any;
+  Row: number = 1;
+  IsShowRow = true;
+
+  ngOnInit(): void {
+    this.id = setInterval(() => {
+      this.Row++;
+      if (this.Row > 10) {
+        clearInterval(this.id);
+        this.IsShowRow = false;
+      }
+    }, 1000);
+  }
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
